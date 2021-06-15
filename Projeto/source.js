3123bddc082bf522a46160aa0080894f6b31e19e
Luana <ltdc01@hotmail.com>
2021-06-15 00:20:12 -0300

    Adoption

--- Projeto/source.js
+++ Projeto/source.js
@@ -5,3 +5,7 @@ function addCart(){
 function scheduling(){
     alert("Horário agendado com sucesso!");
 }
+
+function adopt(){
+    alert("Parabéns! Você anotou seu novo PET! Para levá-lo para o novo lar, dirija-se a nossa loja com a senha: #" + Math.random());
+}
